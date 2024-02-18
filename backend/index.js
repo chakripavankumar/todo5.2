d1c8970d3bb8a1e3e42549d02497eb4403d404f4
unknown <siriyalapavankumar@gmail.com>
2024-02-18 13:41:08 +0530

    db initized

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,32 @@
 const express=require('express');
+const {createTodo}=require('./Types')
 const app= express();
 
 app.use(express.json());
 
 app.post("/todo" , function(req,res){
+    const createPayload=req.body;
+    const prasepayload= createPayload.safeParse(createPayload);
+    if(!prasepayload.success){
+        res.status(411).json({
+            msg:"you sent a wrong inputs"
+        })
+        return;
+    }
 
 })
 app.get("/todos" , function(req,res){
     
 });
 app.put("/completed" , function(req,res){
-    
+    const updatePayload=req.body;
+    const prasePayload= updatePayload.safeParse(updatePayload);
+    if(!prasePayload.success){
+        res.status(411).json({
+            msg:"you sent a wrong inputs"
+        })
+        return;
+    }
 })
 app.listen(3000,()=>{
 console.log("app is listeninggg");
