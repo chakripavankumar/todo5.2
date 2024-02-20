b2ca20c57c5fedc26e2c64d14d516bebbd5ef924
unknown <siriyalapavankumar@gmail.com>
2024-02-20 15:19:58 +0530

    somehow wrapped up

--- frontend/src/components/CreateTodo.jsx
+++ frontend/src/components/CreateTodo.jsx
@@ -1,9 +1,34 @@
+import { useState } from "react"
+
  export function CreateTodo(){
+    const[title, settitle]= useState("");
+    const [ description, setdescription ]= useState("");
     return(
         <div>
-            <input style={{padding:10, margin:10}}type="text" placeholder="title" /> <br></br>
-            <input style={{padding:10, margin:10}} type="text" placeholder="description" /> <br></br>
-            <button  style={{padding:10, margin:10}}>Add todo</button> <br></br>
+            <input style={{padding:10, margin:10}}type="text" placeholder="title" onChange={function(e){
+                 const value = e.target.value
+                 settitle(e.target.value)
+            }} /> <br></br>
+            <input style={{padding:10, margin:10}} type="text" placeholder="description" onChange={function(e){
+                const value= e.target.value
+                setdescription( e.target.value)
+            }} /> <br></br>
+            <button  onClick={()=>{
+                fetch("http://localhost:3000/todo",{
+                    method:"POST",
+                    body: JSON.stringify({
+                        title:title,
+                        description: description
+                    }),
+                    headers:{
+                        "content-type": "application/json"
+                    }
+                }) 
+                .then( async function(res){
+                    const json = await res.json();
+                    alert("todo added")
+                })
+            }} style={{padding:10, margin:10}}>Add todo</button> <br></br>
         </div>
     )
 
